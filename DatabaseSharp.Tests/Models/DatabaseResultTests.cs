8d29af8f717377bdabb8c0f348f6bc4e5f5d14b2
Kristian Skov <kris701kj@gmail.com>
2024-08-22 12:00:18 +0200

    Initial tests

---
+++ DatabaseSharp.Tests/Models/DatabaseResultTests.cs
@@ -0,0 +1,124 @@
+﻿using DatabaseSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSharp.Tests.Models
+{
+	[TestClass]
+	public class DatabaseResultTests
+	{
+		[TestMethod]
+		public void Can_SetDataset()
+		{
+			// ARRANGE
+			var dataset = new DataSet();
+
+			// ACT
+			var result = new DatabaseResult(dataset);
+
+			// ASSERT
+			Assert.AreEqual(dataset, result.DataSet);
+		}
+
+		[TestMethod]
+		public void Can_GetRowCount()
+		{
+			// ARRANGE
+			var dataset = new DataSet();
+			var table = new DataTable();
+			table.Columns.Add(new DataColumn("col1"));
+			table.Columns.Add(new DataColumn("col2"));
+			table.Rows.Add(table.NewRow());
+			table.Rows.Add(table.NewRow());
+			dataset.Tables.Add(table);
+			var result = new DatabaseResult(dataset);
+
+			// ACT
+			var count = result.RowCount();
+
+			// ASSERT
+			Assert.AreEqual(2, count);
+		}
+
+		[TestMethod]
+		public void Can_ContainsColumn()
+		{
+			// ARRANGE
+			var dataset = new DataSet();
+			var table = new DataTable();
+			table.Columns.Add(new DataColumn("col1"));
+			table.Columns.Add(new DataColumn("col2"));
+			table.Rows.Add(table.NewRow());
+			table.Rows.Add(table.NewRow());
+			dataset.Tables.Add(table);
+			var result = new DatabaseResult(dataset);
+
+			// ACT
+
+			// ASSERT
+			Assert.IsTrue(result.ContainsColumn("col1"));
+			Assert.IsTrue(result.ContainsColumn("col2"));
+			Assert.IsFalse(result.ContainsColumn("col3"));
+			Assert.IsFalse(result.ContainsColumn("col4"));
+		}
+
+		[TestMethod]
+		public void Can_GetValue()
+		{
+			// ARRANGE
+			var dataset = new DataSet();
+			var table = new DataTable();
+			table.Columns.Add(new DataColumn("col1", typeof(string)));
+			table.Columns.Add(new DataColumn("col2", typeof(int)));
+			table.Columns.Add(new DataColumn("col3", typeof(DateTime)));
+			table.Columns.Add(new DataColumn("col4", typeof(double)));
+			table.Rows.Add(table.NewRow());
+			table.Rows[0].SetField(table.Columns[0], "abc");
+			table.Rows[0].SetField(table.Columns[1], 123);
+			table.Rows[0].SetField(table.Columns[2], DateTime.MinValue);
+			table.Rows[0].SetField(table.Columns[3], 4.2);
+			table.Rows.Add(table.NewRow());
+			dataset.Tables.Add(table);
+			var result = new DatabaseResult(dataset);
+
+			// ACT
+
+			// ASSERT
+			Assert.AreEqual("abc", result.GetValue<string>("col1"));
+			Assert.AreEqual(123, result.GetValue<int>("col2"));
+			Assert.AreEqual(DateTime.MinValue, result.GetValue<DateTime>("col3"));
+			Assert.AreEqual(4.2, result.GetValue<double>("col4"));
+		}
+
+		[TestMethod]
+		public void Can_GetValueOrNull()
+		{
+			// ARRANGE
+			var dataset = new DataSet();
+			var table = new DataTable();
+			table.Columns.Add(new DataColumn("col1", typeof(string)));
+			table.Columns.Add(new DataColumn("col2", typeof(int)));
+			table.Columns.Add(new DataColumn("col3", typeof(DateTime)));
+			table.Columns.Add(new DataColumn("col4", typeof(double)));
+			table.Rows.Add(table.NewRow());
+			table.Rows[0].SetField(table.Columns[0], "abc");
+			table.Rows[0].SetField(table.Columns[1], 123);
+			table.Rows[0].SetField<DateTime?>(table.Columns[2], null);
+			table.Rows[0].SetField(table.Columns[3], 4.2);
+			table.Rows.Add(table.NewRow());
+			dataset.Tables.Add(table);
+			var result = new DatabaseResult(dataset);
+
+			// ACT
+
+			// ASSERT
+			Assert.AreEqual(123, result.GetValueOrNull<int>("col2"));
+			Assert.IsNull(result.GetValueOrNull<DateTime>("col3"));
+			Assert.AreEqual(4.2, result.GetValueOrNull<double>("col4"));
+		}
+	}
+}
--- DatabaseSharp/Models/DatabaseResult.cs
+++ DatabaseSharp/Models/DatabaseResult.cs
@@ -2,34 +2,70 @@
 
 namespace DatabaseSharp.Models
 {
+	/// <summary>
+	/// Result object from executing a STP
+	/// </summary>
 	public class DatabaseResult
 	{
+		/// <summary>
+		/// Resulting dataset
+		/// </summary>
 		public DataSet DataSet { get; }
+		/// <summary>
+		/// A bool indicating if there are no tables in the dataset
+		/// </summary>
 		public bool IsEmpty { get => DataSet.Tables.Count == 0; }
 
+		/// <summary>
+		/// Main constructor
+		/// </summary>
+		/// <param name="dataSet"></param>
 		public DatabaseResult(DataSet dataSet)
 		{
 			DataSet = dataSet;
 		}
 
+		/// <summary>
+		/// Gets the amount of rows that is contained in a table
+		/// </summary>
+		/// <param name="table"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public int RowCount(int table = 0)
 		{
 			if (DataSet.Tables.Count < table)
-				throw new Exception($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
+				throw new ArgumentOutOfRangeException($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
 			return DataSet.Tables[table].Rows.Count;
 		}
 
+		/// <summary>
+		/// Boolean check if a given column exists.
+		/// </summary>
+		/// <param name="column"></param>
+		/// <param name="table"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public bool ContainsColumn(string column, int table = 0)
 		{
 			if (DataSet.Tables.Count < table)
-				throw new Exception($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
+				throw new ArgumentOutOfRangeException($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
 			return DataSet.Tables[table].Columns.Contains(column);
 		}
 
+		/// <summary>
+		/// Converts a value from the datatable to a ordinary type
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="column"></param>
+		/// <param name="rowID"></param>
+		/// <param name="table"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		/// <exception cref="ArgumentNullException"></exception>
 		public T GetValue<T>(string column, int rowID = 0, int table = 0) where T : IConvertible
 		{
 			if (DataSet.Tables.Count < table)
-				throw new Exception($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
+				throw new ArgumentOutOfRangeException($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
 			object getObj = GetObjectValueFromDataTable(column, rowID, DataSet.Tables[table]);
 
 			if (getObj == null)
@@ -38,10 +74,19 @@ namespace DatabaseSharp.Models
 			return (T)Convert.ChangeType(getObj, typeof(T), System.Globalization.CultureInfo.InvariantCulture);
 		}
 
+		/// <summary>
+		/// Converts a value from the datatable to a ordinary type or null
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="column"></param>
+		/// <param name="rowID"></param>
+		/// <param name="table"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public T? GetValueOrNull<T>(string column, int rowID = 0, int table = 0) where T : struct
 		{
 			if (DataSet.Tables.Count < table)
-				throw new Exception($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
+				throw new ArgumentOutOfRangeException($"Dataset only has {DataSet.Tables.Count} tables, but index '{table}' was requested!");
 			object getObj = GetObjectValueFromDataTable(column, rowID, DataSet.Tables[table]);
 
 			if (getObj == null || DBNull.Value.Equals(getObj))
