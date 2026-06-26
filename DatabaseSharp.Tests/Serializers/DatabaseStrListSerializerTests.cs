711ce4b5a8a7d852bcf5135aa5d46dceb082191d
Kristian Skov <kris701kj@gmail.com>
2026-06-26 09:21:01 +0200

    Added a simple string list serializer

---
+++ DatabaseSharp.Tests/Serializers/DatabaseStrListSerializerTests.cs
@@ -0,0 +1,129 @@
+﻿using DatabaseSharp.Attributes;
+using DatabaseSharp.Helpers;
+using DatabaseSharp.Models;
+using DatabaseSharp.Serializers;
+using DatabaseSharp.Tests.TestModels;
+using System.Data;
+using System.Text.Json;
+
+namespace DatabaseSharp.Tests.Serializers
+{
+	[TestClass]
+	public class DatabaseStrListSerializerTests
+	{
+		public class InputObject
+		{
+			[DatabaseSharp(Serializer = DatabaseStrListSerializer.SerializerName)]
+			public List<string> Col1 { get; set; } = new List<string>();
+			[DatabaseSharp(Serializer = DatabaseStrListSerializer.SerializerName)]
+			public List<string>? Col2 { get; set; } = null;
+		}
+
+		public class InputObject2
+		{
+			public string Other { get; set; }
+			[DatabaseSharp(Serializer = DatabaseStrListSerializer.SerializerName)]
+			public List<string> Col1 { get; set; } = new List<string>();
+		}
+
+		[TestMethod]
+		public void Can_Serialize()
+		{
+			// ARRANGE
+			var serializers = new Dictionary<string, IDatabaseSerializer>()
+			{
+				{ DatabaseStrListSerializer.SerializerName, new DatabaseStrListSerializer() }
+			};
+			var input = new InputObject()
+			{
+				Col1 = new List<string>()
+				{
+					"ab",
+					"35u90af",
+					"aaaaa"
+				},
+				Col2 = null
+			};
+			var expected1 = string.Join(';', input.Col1);
+
+			// ACT
+			var param = ParameterHelpers.GenerateParametersFromObject(input, serializers);
+
+			// ASSERT
+			Assert.IsNotNull(param);
+			Assert.AreEqual(2, param.Count);
+			Assert.AreEqual((param[0] as SQLParam)!.Value, expected1);
+			Assert.AreEqual((param[1] as SQLParam)!.Value, null);
+		}
+
+		[TestMethod]
+		public void Can_Serialize2()
+		{
+			// ARRANGE
+			var serializers = new Dictionary<string, IDatabaseSerializer>()
+			{
+				{ DatabaseDefaultSerializer.SerializerName, new DatabaseDefaultSerializer() },
+				{ DatabaseStrListSerializer.SerializerName, new DatabaseStrListSerializer() }
+			};
+			var input = new InputObject2()
+			{
+				Other = "some val",
+				Col1 = new List<string>()
+				{
+					"1",
+					"aaaaa   QKGS"
+				}
+			};
+			var expected1 = string.Join(';', input.Col1);
+
+			// ACT
+			var param = ParameterHelpers.GenerateParametersFromObject(input, serializers);
+
+			// ASSERT
+			Assert.IsNotNull(param);
+			Assert.AreEqual(2, param.Count);
+			Assert.AreEqual((param[0] as SQLParam)!.Value, input.Other);
+			Assert.AreEqual((param[1] as SQLParam)!.Value, expected1);
+		}
+
+		[TestMethod]
+		public void Can_Deserialize()
+		{
+			// ARRANGE
+			var serializers = new Dictionary<string, IDatabaseSerializer>()
+			{
+				{ DatabaseStrListSerializer.SerializerName, new DatabaseStrListSerializer() }
+			};
+
+			var input = new InputObject()
+			{
+				Col1 = new List<string>()
+				{
+					"ab",
+					"35u90af",
+					"aaaaa"
+				},
+				Col2 = null
+			};
+			var input1 = string.Join(';', input.Col1);
+
+			var dataset = new DataSet();
+			var table = new DataTable();
+			table.Columns.Add(new DataColumn("Col1", typeof(string)));
+			table.Columns.Add(new DataColumn("Col2", typeof(string)));
+			table.Rows.Add(table.NewRow());
+			table.Rows[0].SetField<string>(table.Columns[0], input1);
+			table.Rows[0].SetField<string?>(table.Columns[1], null);
+			dataset.Tables.Add(table);
+			var result = new DatabaseResult(dataset, serializers);
+
+			// ACT
+			var row = result[0][0];
+			var model = row.Fill<InputObject>();
+
+			// ASSERT
+			Assert.AreEqual(input1, string.Join(';', model.Col1));
+			Assert.AreEqual(null, model.Col2);
+		}
+	}
+}
---
+++ DatabaseSharp/Serializers/DatabaseStrListSerializer.cs
@@ -0,0 +1,71 @@
+﻿using DatabaseSharp.Attributes;
+using DatabaseSharp.Models;
+using System.Collections;
+using System.Reflection;
+using System.Text.Json;
+
+namespace DatabaseSharp.Serializers
+{
+	/// <summary>
+	/// This is a serializer that supports converting a concatinated string in the database to a list type in C#
+	/// </summary>
+	public class DatabaseStrListSerializer : IDatabaseSerializer
+	{
+		/// <summary>
+		/// Name of the string list serializer
+		/// </summary>
+		public const string SerializerName = "STRL";
+
+		public Type? OverrideDatabaseType { get; } = typeof(string);
+		public char Seperator { get; set; } = ';';
+		public dynamic? Deserialise(PropertyInfo propInfo, DatabaseResultRow row, DatabaseResult source)
+		{
+			var columnName = propInfo.Name;
+			var underlying = Nullable.GetUnderlyingType(propInfo.PropertyType);
+			if (propInfo.GetCustomAttribute<DatabaseSharpAttribute>() is DatabaseSharpAttribute overrideAttribute)
+			{
+				if (overrideAttribute.ColumnName != null)
+					columnName = overrideAttribute.ColumnName;
+			}
+			var value = row.GetValue<string>(columnName);
+			if (value == null || value == "")
+				return null;
+			var item = ReturnAsStringOrNull(value);
+			return item;
+		}
+
+		private dynamic? ReturnAsStringOrNull(object? value)
+		{
+			if (value == null)
+				return null;
+			if (value is string sValue)
+				return sValue.Split(Seperator).ToList();
+			return new List<string>();
+		}
+
+		public ISQLParameter Serialize(object item, PropertyInfo propInfo)
+		{
+			var value = propInfo.GetValue(item);
+			var parameterName = propInfo.Name;
+			if (propInfo.GetCustomAttribute<DatabaseSharpAttribute>() is DatabaseSharpAttribute overrideName)
+			{
+				if (overrideName.ParameterName != null)
+					parameterName = overrideName.ParameterName;
+			}
+
+			if (value == null)
+				return new SQLParam(parameterName, null);
+			if (value is IList lst)
+			{
+				var asStr = "";
+				foreach(var lstItem in lst)
+					asStr += lstItem.ToString() + Seperator;
+				if (asStr.Length > 0)
+					asStr = asStr.Substring(0, asStr.Length - 1);
+				return new SQLParam(parameterName, asStr);
+			}
+			else
+				throw new Exception("Property should be a string list!");
+		}
+	}
+}
